18200b0f257ae630fbfd1cd516c9923643554ae3
Aryan Rawat <66024933+aryanrawat229@users.noreply.github.com>
2025-03-10 00:01:21 +0530

    Create concat.js

---
+++ JS assignment 4/concat.js
@@ -0,0 +1,8 @@
+// Concatenate the Strings
+function Concatenate_Strings(str1,str2) {
+    let str3 = str1.concat(" ",str2)
+    return(str3);
+}
+let str1 ="I  am learning javascript";
+let str2 ="and react js"
+console.log(Concatenate_Strings(str1,str2));
