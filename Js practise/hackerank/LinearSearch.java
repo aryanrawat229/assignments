2322da3abd99bc5da31e6295ff6d848a24d00559
Aryan Rawat <66024933+aryanrawat229@users.noreply.github.com>
2025-06-01 23:56:09 +0530

    Create LinearSearch.java

---
+++ Js practise/hackerank/LinearSearch.java
@@ -0,0 +1,8 @@
+public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Return the index if found
+            }
+        }
+        return -1; // Return -1 if not found
+    }
